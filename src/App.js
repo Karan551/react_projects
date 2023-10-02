66f131f5735dc42e1d2eefbca4def400591c5bf7
Kartik <iamkaranmishra551@gmail.com>
2023-10-02 11:40:24 +0530

    Adding Bootstrap in Our textutils app.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@
 import './App.css';
 
 function App() {
-  let name = "Ganesh";
-  let role = 'Software Engineer'
-  const avatar="https://cdn.pixabay.com/photo/2023/09/15/04/05/sea-8254024_640.jpg"
+  // Adding navbar component with the help of bootstrap.
   return (<>
-    <h1>My Name is :{name}, And My Role is : {role}</h1>
-    <div className="name">This is my first Webpage On React.</div>
-    <img src={avatar} alt="Image via pixabay." />
-
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">Navbar</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Link</a>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
   </>
   );
 }
