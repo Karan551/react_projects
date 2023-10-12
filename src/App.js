ba138ded9a49d9689a8bdb7a6a30f0ca7d8a60ba
Kartik <iamkaranmishra551@gmail.com>
2023-10-12 07:28:55 +0530

    adding router and about page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 import React, { useState } from 'react';
+// import { BrowserRouter as Router,Route,Link,Routes,Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   // create a state variable to show mode.
@@ -29,26 +31,38 @@ function App() {
       document.body.style.backgroundColor = '#042743'
       showAlert("Dark Mode has been enabled.", "dark")
       // change title on runtime
-      document.title="Text Utils - Dark Mode"
+      document.title = "Text Utils - Dark Mode"
     } else {
       setMode('light')
       document.body.style.backgroundColor = 'white'
       showAlert("Light Mode has been enabled.", "light")
-      document.title="Text Utils - Light Mode"
+      document.title = "Text Utils - Light Mode"
     }
   }
 
   // Adding navbar component with the help of bootstrap.
-  return (<>
-    <Navbar title='TextUtils' about='About Us' mode={mode} toogleMode={toogleMode} />
-    <div className="container my-3">
-      <Alert alert={alert} />
-      <TextForm heading='Enter Text Here:' mode={mode} showAlert={showAlert} />
-    </div>
-    {/* <div className="container my-3">
-      <About />
-    </div> */}
-  </>
+  return (
+    <>
+      <BrowserRouter >
+        {/* This is Navbar component */}
+
+        <Navbar title='TextUtils' about='About Us' mode={mode} toogleMode={toogleMode} />
+        <div className="container my-2">
+          {/* This is alert component. */}
+          <Alert alert={alert} />
+          <Routes>
+            {/* This is TextForm component */}
+            <Route path="/" element={<TextForm heading='Enter Text Here:' mode={mode} showAlert={showAlert} />} />
+
+            {/* this is about component. */}
+            <Route path='/about' element={<About />}>
+              {/*  */}
+            </Route>
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+    </>
   );
 }
 
